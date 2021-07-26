5ef9e05e1d040b135258259bcdd40c75c2c6a78c
Higor Rocha <you@example.com>
2021-07-26 11:10:21 -0300

    Add ScrollView

--- lib/Home.dart
+++ lib/Home.dart
@@ -25,74 +25,76 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.orange,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: Image.asset("images/logo.jpg"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                  "Find out which fuel is the best choice for your car.",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold
-                ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Image.asset("images/logo.jpg"),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Alcohol Price, ex: 2.58"
-              ),
-              style: TextStyle(
-                fontSize: 16
-              ),
-              controller: _controllerAlcohol,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Gasoline Price, ex: 5.89"
-              ),
-              style: TextStyle(
-                fontSize: 16
-              ),
-              controller: _controllerGasoline,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.orange,
-                  onSurface: Colors.orangeAccent,
-                  elevation: 5,
-                  padding: EdgeInsets.all(15)
-                ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
                 child: Text(
-                    "Calculate",
+                  "Find out which fuel is the best choice for your car.",
                   style: TextStyle(
-                    fontSize: 18
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
                   ),
                 ),
-                onPressed: (){},
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
-              child: Text(
-                  "Result",
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Alcohol Price, ex: 2.58"
+                ),
                 style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange
+                    fontSize: 16
+                ),
+                controller: _controllerAlcohol,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Gasoline Price, ex: 5.89"
+                ),
+                style: TextStyle(
+                    fontSize: 16
+                ),
+                controller: _controllerGasoline,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      onSurface: Colors.orangeAccent,
+                      elevation: 5,
+                      padding: EdgeInsets.all(15)
+                  ),
+                  child: Text(
+                    "Calculate",
+                    style: TextStyle(
+                        fontSize: 18
+                    ),
+                  ),
+                  onPressed: (){},
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Text(
+                  "Result",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
